5babfc4ad7b864e2c1cfe17e23b566edcde3db2e
Overloadedq <artleonid20022509@gmail.com>
2025-10-27 08:38:55 +0300

    -- Add class that read words from file(words.txt) and generate random word

--- src/Logic/Hangman.java
+++ src/Logic/Hangman.java
@@ -1,9 +1,10 @@
 package Logic;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Hangman {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         StartsApp starts = new StartsApp();
         starts.ShowStartMessage();
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +18,9 @@ public class Hangman {
                 break;
         }
 
-
+        ListWords file = new ListWords();
+        String filepath = "resources/words.txt";
+        file.PrintWords(filepath);
 
 
         scanner.close();
---
+++ src/Logic/ListWords.java
@@ -0,0 +1,34 @@
+package Logic;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+class ListWords {
+    public void PrintWords(String filepath) throws IOException
+    {
+        List<String> words = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(filepath)))
+        {
+            String line;
+            while((line=br.readLine())!=null)
+            {
+                words.add(line);
+            }
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        String[] wordArray = words.toArray(new String[0]);
+        Random rand = new Random();
+        int random_index = rand.nextInt(wordArray.length);
+        String word = wordArray[random_index];
+        System.out.println(word);
+    }
+
+}
